5a98b3eeba05904dc2bbd75aee7181053403a611
Zi <68175187+zzzilya@users.noreply.github.com>
2022-11-23 14:49:21 +0900

    Upload all files

---
+++ resources/js/footer.js
@@ -0,0 +1,4 @@
+// Footer
+const year = new Date().getFullYear();
+const footer = document.querySelector(".footer");
+footer.innerHTML = `Copyright &copy; ${year} Gibatullina Zilfira`;
---
+++ resources/js/script.js
@@ -0,0 +1,13 @@
+$("#lime-btn").click(function () {
+  $("#lime").slideToggle("slow");
+});
+
+$("#earth-tech-btn").click(function () {
+  $("#earth-tech").slideToggle("slow");
+});
+$("#jal-btn").click(function () {
+  $("#jal").slideToggle("slow");
+});
+$("#busena-btn").click(function () {
+  $("#busena").slideToggle("slow");
+});
